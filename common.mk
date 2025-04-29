6639c6db39eea25bb9c96c246b02d9e934d3befc
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:37:50 +0100

    sm7325-common: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da

--- common.mk
+++ common.mk
@@ -192,6 +192,11 @@ PRODUCT_ENABLE_UFFD_GC := false
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/device/force_charger_suspend)
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.motorola_lahaina
