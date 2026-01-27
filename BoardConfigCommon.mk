3200833b88e4dd869ba4ae70e4989202e2f885f7
Nolen Johnson <johnsonnolen@gmail.com>
2026-01-26 21:21:46 -0500

    sm7325-common: Remove now duplicated sepolicy
    * Duplicated in device/lineage/sepolicy.
    
    Change-Id: Ic22b0b381240a7da7654fddb261230a2ef721c0c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -141,7 +141,6 @@ ENABLE_VENDOR_RIL_SERVICE := true
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/motorola/sepolicy/qti/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
